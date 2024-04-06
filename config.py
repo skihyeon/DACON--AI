4b9c193f0fdba0e8fa48d4581b9ffc3c71696fb7
skihyeon <hero981001@gmail.com>
2024-04-06 16:57:00 +0900

    update config using name

--- config.py
+++ config.py
@@ -1,17 +1,19 @@
 import torch
 
+name = "gpu"
+
 class Config:
     class general:
         seed = 456
         device = "cuda" if torch.cuda.is_available() else "cpu"
 
-        project_name_pt = "deepSVDD_pretrain_new"
-        project_name = "deepSVDD_new"
+        project_name_pt = f"deepSVDD_pretrain_{name}"
+        project_name = f"deepSVDD_{name}"
         entity_name = "hero981001"
         
     class train:
         train_csv = './datas/train.csv'
-        model_save_path = './model_files/deepSVDD/v1_test.pt'
+        model_save_path = f'./model_files/deepSVDD/v1_{name}.pt'
         load_batch_size = 16
 
     class test:
@@ -20,7 +22,7 @@ class Config:
     class model:
         class deepSVDD:
             pretrained = True
-            pretrained_path = './model_files/deepSVDD/pretrained_params_test.pth'
+            pretrained_path = f'./model_files/deepSVDD/pretrained_params_{name}.pth'
             num_epochs=50
             lr=1e-4
             weight_decay=0.5e-6
@@ -29,22 +31,22 @@ class Config:
             gamma = 0.1
         
         class C_AE:
-            latent_dim = 512
+            latent_dim = 2048
             num_epochs = 100
             weight_decay = 0.5e-3
             lr = 1e-3
             lr_milestones=[50]
             step_size = 10
             gamma = 0.99
-            save_params_path = './model_files/deepSVDD/pretrained_params_test.pth'
-            save_model_path = './model_files/deepSVDD/C_AE_test.pt'
+            save_params_path = f'./model_files/deepSVDD/pretrained_params_{name}.pth'
+            save_model_path = f'./model_files/deepSVDD/C_AE_{name}.pt'
 
     
     class inference:
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        pretrained_path = './model_files/deepSVDD/pretrained_params_test.pth'
-        model_path = './model_files/deepSVDD/v1_test.pt'
+        pretrained_path = f'./model_files/deepSVDD/pretrained_params_{name}.pth'
+        model_path = f'./model_files/deepSVDD/v1_{name}.pt'
         test_file = "./datas/test.csv"
         batch_size = 64
-        save_results_path = "./results/v1_test_result.csv"
-        submit_path = './submits/v1_test_submit.csv'
+        save_results_path = f"./results/v1_{name}_result.csv"
+        submit_path = f'./submits/v1_{name}_submit.csv'
